0d5aeb994046156e8ed04215c436b39893ad075b
Ashif Ali <ashif9720@gmail.com>
2026-08-28 11:58:15 +0530

    Update Firebase configuration for new project
    Updated Firebase configuration with new project details.

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,13 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT.firebasestorage.app",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAHwV9fDsRXKjiImYuIF-qhi1hQ3MlqKng",
+  authDomain: "skillearn-hub.firebaseapp.com",
+  projectId: "skillearn-hub",
+  storageBucket: "skillearn-hub.firebasestorage.app",
+  messagingSenderId: "368756239597",
+  appId: "1:368756239597:web:57278e2e36bc6e99e730e4",
+  measurementId: "G-B7S23BPQ9T"
 };
-
-firebase.initializeApp(firebaseConfig);
-
-const auth = firebase.auth();
-const db = firebase.firestore();

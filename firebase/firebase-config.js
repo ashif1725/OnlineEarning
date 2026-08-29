60625452c8b47f79c788243affda03b4b0ee56bc
Ashif Ali <ashif9720@gmail.com>
2026-08-30 03:23:06 +0530

    Update Firebase configuration with project details
    Updated Firebase configuration with actual API key and project details.

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -13,7 +13,16 @@
 "use strict";
 
 const firebaseConfig = {
-apiKey: "YOUR_FIREBASE_API_KEY",
+apiKey: "// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAHwV9fDsRXKjiImYuIF-qhi1hQ3MlqKng",
+  authDomain: "skillearn-hub.firebaseapp.com",
+  projectId: "skillearn-hub",
+  storageBucket: "skillearn-hub.firebasestorage.app",
+  messagingSenderId: "368756239597",
+  appId: "1:368756239597:web:57278e2e36bc6e99e730e4",
+  measurementId: "G-B7S23BPQ9T"
+};",
 authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
 projectId: "YOUR_PROJECT_ID",
 storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
